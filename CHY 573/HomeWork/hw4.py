b1ce693bb36ea00681df8c7ac59d3a346225ec6a
TravisCzechorskiUMaine <85692199+TravisCzechorskiUMaine@users.noreply.github.com>
2021-10-06 14:08:54 -0400

    Add files via upload

---
+++ CHY 573/HomeWork/hw4.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+
+Created on Mon Oct  4 22:47:42 2021
+
+@author: Travis J Czechorski
+
+Github: https://github.com/tjczec01
+
+Email:   travis.czechorski@maine.edu
+         tjczec01@gmail.com
+        
+Advisor: thomas.schwartz@maine.edu
+       
+Github:  https://github.com/tjczec01 
+         https://github.com/TravisCzechorskiUMaine
+
+"""
+
+sw = 1000 # tones/day
+swkg = 1000 * sw  # kg/day
+liqkg = swkg * 5
+# print(swkg, liqkg)
+lig_0 = 0.3 * swkg
+solution = 1838750
+print(220000/solution)
+print((144750*1000)/solution)
